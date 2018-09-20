d33a69fcafdd593d59d32e6ecda691b86315d4ed
Lee Goddard <leegee@gmail.com>
2018-09-20 13:42:01 +0200

    tidy

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,23 @@ module.exports = {
     devServer: {
         inline: true
     },
+    devServer: {
+        publicPath: "/",
+        contentBase: "./public/",
+        hot: true
+    },
     devtool: "inline-source-map",
     entry: "./src/main.ts",
     output: {
-        path: path.resolve(__dirname, "build"),
-        publicPath: "/assets/",
+        path: path.resolve(__dirname, "public"),
+        publicPath: "/build/",
         filename: "bundle.js"
     },
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: [".ts", ".tsx", ".js"]
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             { test: /\.tsx?$/, loader: "ts-loader" }
         ]
     }
