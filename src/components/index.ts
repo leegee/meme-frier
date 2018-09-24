8af643ea29e2302e29b6749e5db04496f7fa23b6
Lee Goddard <leegee@gmail.com>
2018-09-24 23:09:44 +0200

    wip@

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import { MemeFrier } from './Frier';
 import { FriedMeme } from './FriedMeme';
+import { RangedInput } from './RangedInput';
 
 customElements.define('meme-frier', MemeFrier);
 customElements.define('fried-meme', FriedMeme);
+customElements.define('ranged-input', RangedInput);
