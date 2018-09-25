4de5169529d53b93b7c2f741b26fa98a681c43b5
Lee Goddard <leegee@gmail.com>
2018-09-25 09:46:44 +0200

    Style

--- src/components/RangedInput.ts
+++ src/components/RangedInput.ts
@@ -2,7 +2,6 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element';
 
 export class RangedInput extends PolymerElement {
     value!: string | number;
-    vertical!: boolean;
     label!: string;
 
     static get properties() {
@@ -12,52 +11,40 @@ export class RangedInput extends PolymerElement {
             id: { type: String },
             min: { type: Number },
             max: { type: Number },
-            step: { type: Number },
-            vertical: { type: Boolean, reflectToAttribute: true },
-            class: {
-                type: String,
-                value: () => {
-                    return this.hasOwnProperty('vertical') ? 'slider-wrapper-v' : ''
-                }
-            }
+            step: { type: Number }
         };
     }
 
     static get template() {
         return html`
         <style>
-           .slider-wrapper {
+            :host {
+                border: 1px solid gray;
+                display: inline-block;
                 text-align: center;
-                display: block;
-                border: 1px solid red;
-                float: left;
+                margin: 0;
                 padding: 0;
                 width: 150pt;
                 height: 40pt;
-                margin: 0;
             }
-            .slider-wrapper-v {
-                background: red;
+            :host(.vertical) {
                 width: 40pt;
                 height: 150pt;
-                transform-origin: 75px 75px;
+                transform-origin: 75pt 75pt;
                 transform: rotate(-90deg);
             }
-            ..slider-wrapper label, .slider-wrapper input[type=range] {
-                display: block;
+            label, input {
+                display: inline-block;
             }
         </style>
-        <div class="slider-wrapper" id="[[id]]">
-            <label> 
-                {{label}}
-                <input class="{{class}}" on-input="inputHandler" type="range" value="{{value::input}}" min="[[min]]" max="[[max]]" step="[[step]]">
-            </label>
-        </div>
+        <label> 
+            {{label}}
+            <input on-input="inputHandler" type="range" value="{{value::input}}" min="[[min]]" max="[[max]]" step="[[step]]">
+        </label>
         `;
     }
 
     inputHandler() {
-        console.log(`Change to ${this.label}`, this.value)
         this.dispatchEvent(new CustomEvent('change', { detail: { value: this.value } }));
     }
 }
